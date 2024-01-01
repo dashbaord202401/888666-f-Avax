1597681eb32fe1427f4f33e765d25fbabad961eb
Akash-Kadu <138386707+Akash-Kadu@users.noreply.github.com>
2024-01-01 22:48:35 +0530

    Update Solidity_Errors.sol

--- Solidity_Errors.sol
+++ Solidity_Errors.sol
@@ -1,17 +1,26 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-contract Solidity_Errors {
-    uint256 a=1;
-    function requireF(uint256 _Xint) public pure {
-        require(_Xint > 720, "Input must be greater than 720");
-    }
-    function assertF() public view {
-        assert(a == 0); //It takes only one parameter
-    }
-    function revertF(uint256 _Xint) public pure {
-        if (_Xint < 700) {
-            revert("Input Must be greater than 700");
+contract ETC_S {
+    uint256 public SL ;
+
+    function Decide_val(uint256 X_Val) public {
+        // Require is used to validate a condition
+        require(X_Val > 100 , "Value must be greater than 100");
+
+        // Using assert() to ensure an internal condition
+        //and to check the invariants
+        assert(X_Val<1000);
+
+        // Setting the value
+        if(X_Val == 150){
+            // Using revert() to revert the transaction
+        revert("This operation is not allowed"); 
         }
+        SL= X_Val;
+        
+        
     }
+
+    
 }
